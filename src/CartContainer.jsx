a8921b715b043f92ec9e886f91062c8c994d89ec
abuzar <abuzardawar444@gmail.com>
2024-11-01 21:50:07 +0500

    commit

--- src/CartContainer.jsx
+++ src/CartContainer.jsx
@@ -3,28 +3,30 @@ import { useGlobalContext } from './context';
 import cartItems from './data';
 const CartContainer = () => {
   const { cart, clearCart, totalCost } = useGlobalContext();
+
   const cartArray = Array.from(cart.entries());
 
   if (cartArray.length === 0) {
     return (
-      <section className="cart">
+      <section className='cart'>
         {/* cart header */}
         <header>
           <h2>your bag</h2>
-          <h4 className="empty-cart">is currently empty</h4>
+          <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
     );
   }
   return (
-    <section className="cart">
+    <section className='cart'>
       {/* cart header */}
       <header>
         <h2>your bag</h2>
       </header>
       {/* cart items */}
       <div>
-        {cartArray.map(([id, item]) => {
+        {cartArray.map((cartItem) => {
+          const [id, item] = cartItem;
           return <CartItem key={id} {...item} />;
         })}
       </div>
@@ -32,11 +34,11 @@ const CartContainer = () => {
       <footer>
         <hr />
         <div>
-          <h5 className="cart-total">
-            total <span>${totalCost}</span>
+          <h5 className='cart-total'>
+            total <span>${totalCost.toFixed(2)}</span>
           </h5>
         </div>
-        <button className="btn btn-hipster" onClick={clearCart}>
+        <button className='btn btn-hipster' onClick={clearCart}>
           clear cart
         </button>
       </footer>
